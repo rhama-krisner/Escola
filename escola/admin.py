1e178fdb4bb98259a005775d2b267c734058c2db
Rhama Krisner <rhamakrisner@gmail.com>
2021-09-27 21:46:30 -0300

    Update admin.py

--- escola/admin.py
+++ escola/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from escola.models import Aluno, Curso
 
-# Register your models here.
+class Alunos(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'rg', 'cpf', 'data_nascimento')
+    list_display_links = ('id', 'nome')
+    search_fields = ('nome',)
+    list_per_page = 20
+
+admin.site.register(Aluno, Alunos)
+
+class Cursos(admin.ModelAdmin):
+    list_display = ('id', 'codigo_Curso', 'descricao')
+    list_display_links = ('id', 'codigo_Curso')
+    search_fields = ('codigo_Curso',)
+
+admin.site.register(Curso, Cursos)
