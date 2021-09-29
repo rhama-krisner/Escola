ec2379a182c3b1c02b678150d868b61609c60d50
Rhama Krisner <rhamakrisner@gmail.com>
2021-09-29 17:49:10 -0300

    Update serializer.py

--- escola/serializer.py
+++ escola/serializer.py
@@ -1,12 +1,12 @@
 from rest_framework import serializers
 from .models import Aluno, Curso
 
-class Aluno_Serializer(serializers.ModelSerializer):
+class AlunoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Aluno
         fields = '__all__'
 
-class Curso_Serializer(serializers.ModelSerializer):
+class CursoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Curso
         fields = '__all__'
